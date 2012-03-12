ee5651847c63d2308288ac452b870bd1e8a5d0e7
Tom Xue <tomxue026@gmail.com>
2012-03-12 15:07:51 +0000

    to show that 'max' is an existing function with <algorithm>

--- example.cpp
+++ example.cpp
@@ -1,20 +1,13 @@
+// max example
+// max is an existing function in <algorithm>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-template <class T> 	//class也可用typename替换
-T mymax(T a, T b) 	//参数和返回值都是T类型
-{
- return a > b? a: b;
-}
-
-int main()
-{
- cout << mymax(1, 2) << endl; 			//隐式调用int类型的mymax
- cout << mymax(1.1f, 2.2f) << endl; 	//隐式调用float类型的mymax
- cout << mymax(1.11l, 2.22l) << endl; 	//隐式调用double类型的mymax
- cout << mymax('A', 'C') << endl; 		//隐式调用char类型的mymax
- cout << mymax<int>(1, 2.0) << endl; 	//必须指定int类型
-
- return 0;
+int main () {
+  cout << "max(1,2)==" << max(1,2) << endl;
+  cout << "max(2,1)==" << max(2,1) << endl;
+  cout << "max('a','z')==" << max('a','z') << endl;
+  cout << "max(3.14,2.73)==" << max(3.14,2.73) << endl;
+  return 0;
 }
